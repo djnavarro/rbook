cddbff141ad68188b0c7e428daa79eedcc8486be
Danielle Navarro <djnavarro@protonmail.com>
2023-07-11 15:30:36 +1000

    adds status notes

--- build/common.R
+++ build/common.R
@@ -17,3 +17,34 @@ knitr::knit_hooks$set(output = function(x, options) {
     hook_output(x, options)
   }
 })
+
+
+
+# status note -------------------------------------------------------------
+
+status <- function(type) {
+  status <- switch(type,
+                   polishing = "should be readable but is currently undergoing final polishing",
+                   restructuring = "is undergoing heavy restructuring and may be confusing or incomplete",
+                   drafting = "is currently a mess, and I don't recommend reading it",
+                   complete = "is largely complete and just needs final proof reading",
+                   stop("Invalid `type`", call. = FALSE)
+  )
+  
+  class <- switch(type,
+                  polishing = "note",
+                  restructuring = "important",
+                  drafting = "important",
+                  complete = "note"
+  )
+  
+  callout <- paste0(
+    "\n",
+    "::: {.callout-", class, "} \n",
+    "You are, I'm sorry to inform you, reading the work-in-progress revision of \"Learning Statistics with R\". ",
+    "This chapter ", status, ". \n",
+    "::: \n"
+  )
+  
+  cat(callout)
+}
